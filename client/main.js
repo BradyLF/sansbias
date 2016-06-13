da637d79627d13b6a5323df11667b569cd741024
Brady Africk <bradyafrick@gmail.com>
2016-06-13 16:59:48 -0700

    completed basic functionality

--- client/main.js
+++ client/main.js
@@ -2,7 +2,6 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
-//routes for different templates
 
 //main template
 Router.route('/', function () {
@@ -24,13 +23,29 @@ Router.route('/joinRoom/:_id', function () {
   this.render('joinRoom');
 });
 
-//TODO: display template 
+// display template 
 Router.route('/room/:_id', function () {
   var params = this.params;
   var roomID = params._id;
   
-  console.log(Rooms.findOne({_id: roomID}));
+  console.log(roomID);
+  
   this.render("displayRoom");
+});
+
+// display admin template 
+Router.route('/manageRoom/:_id', function () {
+  var params = this.params;
+  var roomID = params._id;
+  
+  console.log(roomID);
+  
+  this.render("manageRoom");
+});
+
+//about template 
+Router.route('/about', function () {
+  this.render('about');
 });
 
 //make sure you're client side
@@ -45,6 +60,9 @@ if (Meteor.isClient) {
 			//get creator of room
 			var admin = $('.adminName').val();
 			
+			//get room name
+			var roomName = $('.roomName').val();
+			
 			//make an array of the rooms members
 			var members = [admin];
 			
@@ -54,14 +72,25 @@ if (Meteor.isClient) {
 			//timestamp on submit
 			var timeStamp = Math.floor(Date.now() / 1000);
 			
+			//funtion that generates a random alphanumeric code
+			function stringGen(len){
+				var text = "";
+				var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+				for( var i=0; i < len; i++ )
+					text += charset.charAt(Math.floor(Math.random() * charset.length));
+				return text;
+				}
+			
 			//add relevant infor to collection
 			Rooms.insert({
 				optionsList: options,
 				optionsArr: options.split(','),
 				admin: admin,
+				adminKey: stringGen(10),
 				membersArr: members,
+				roomName: roomName,
 				isOpen: isOpen,
-				finalChoice: "Not Yet Made",
+				finalChoice: "A choice will be made when the room is closed",
 				timeStamp: timeStamp
 			});
 			
@@ -71,7 +100,7 @@ if (Meteor.isClient) {
 			
 			//displays the newly generated roomID
 			var newRoomID = Rooms.findOne({}, {sort: {timeStamp: -1}});
-			document.getElementById("roomID").innerHTML = newRoomID._id.toString()
+			window.location.href = '/manageRoom/' + newRoomID.adminKey.toString();
 		}
 	});
 	
@@ -83,7 +112,7 @@ if (Meteor.isClient) {
 			var getRoomID = $('.joinID').val();
 			
 			//get the roomID
-			var newRoomMember = $('.memberName').val();
+			var newRoomMember = " " + $('.memberName').val();
 			
 			//add new member
 			var newArr = Rooms.findOne({_id: getRoomID}).membersArr;
@@ -95,8 +124,10 @@ if (Meteor.isClient) {
 				{ $set: { "membersArr" : newArr } }
 			);
 			
+			window.location.href = '/room/' + getRoomID;
 		}
 	});
+	
 	
 	//get parameters in a join url
 	Template.joinRoom.helpers({
@@ -105,14 +136,88 @@ if (Meteor.isClient) {
 			return params && params._id ? params._id : '';
 		}
     });
+    
+    //get parameters in a display url
+	Template.displayRoom.helpers({
+		//display room name
+        roomName: function () {
+			var params =  Router.current().params;			
+			var getRoomName = Rooms.findOne({_id: params._id}).roomName.toString();		
+			return getRoomName;
+		},
+		//display roomID
+		roomID: function () {
+			var getRoomLink = window.location.href;			
+			return getRoomLink;
+		},
+		//display admin name
+		adminName: function () {
+			var params =  Router.current().params;			
+			var getAdminName = Rooms.findOne({_id: params._id}).admin.toString();		
+			return getAdminName;
+		},
+		//display roomMembers
+		roomMembers: function () {
+			var params =  Router.current().params;			
+			var roomMembers = Rooms.findOne({_id: params._id}).membersArr;
+			return roomMembers;
+		},
+		//display options
+		options: function () {
+			var params =  Router.current().params;			
+			var options = Rooms.findOne({_id: params._id}).optionsArr;
+			return options;
+		},
+		choice: function () {
+			var params =  Router.current().params;			
+			var getChoice = Rooms.findOne({_id: params._id}).finalChoice.toString();		
+			return getChoice;
+		},
+    });
 	
-	//TODO - get room ID from url parameters
-	//Template.displayRoom.helpers({
-	//	people: function() {
-	//		var getRoomID = $('.joinID').val();
-	//		return Rooms.findOne({_id: "9GAJubXhkd32HKDHb"}).membersArr;
-	//	}
-	//});
-	
-	
+	//get parameters in a manage url
+	Template.manageRoom.helpers({
+		//display room name
+        roomName: function () {
+			var params =  Router.current().params;			
+			var getRoomName = Rooms.findOne({adminKey: params._id}).roomName.toString();		
+			return getRoomName;
+		},
+		//display adminLink
+		adminLink: function () {
+			var getAdminLink = window.location.href;			
+			return getAdminLink;
+		},
+		//display roomID
+		joinLink: function () {
+			var params =  Router.current().params;			
+			var getRoomLink = Rooms.findOne({adminKey: params._id})._id.toString();
+			var url = location.origin = location.protocol + "//" + location.host;	
+			console.log()	
+			return url + "/joinRoom/" + getRoomLink;
+		},
+		//display admin name
+		adminName: function () {
+			var params =  Router.current().params;			
+			var getAdminName = Rooms.findOne({adminKey: params._id}).admin.toString();		
+			return getAdminName;
+		},
+		//display roomMembers
+		roomMembers: function () {
+			var params =  Router.current().params;			
+			var roomMembers = Rooms.findOne({adminKey: params._id}).membersArr;
+			return roomMembers;
+		},
+		//display options
+		options: function () {
+			var params =  Router.current().params;			
+			var options = Rooms.findOne({adminKey: params._id}).optionsArr;
+			return options;
+		},
+		choice: function () {
+			var params =  Router.current().params;			
+			var getChoice = Rooms.findOne({adminKey: params._id}).finalChoice.toString();		
+			return getChoice;
+		},
+    });
 }
