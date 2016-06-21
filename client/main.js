dfd687930607d6cc6da7765bbd7d6f762b241185
Brady Africk <bradyafrick@gmail.com>
2016-06-20 20:05:48 -0700

    began on client submission

--- client/main.js
+++ client/main.js
@@ -11,6 +11,8 @@ import './main.html';
 //ROUTES
 //main template
 
+Meteor.subscribe("rooms");
+
 Router.route('/', function () {
 	this.render('index');
 });
@@ -31,7 +33,7 @@ Router.route('/joinRoom/:_id', function () {
 });
 
 // display template 
-Router.route('/room/:_id', function () {
+Router.route('/room/:_id/:personID', function () {
 	//window.onbeforeunload = function() {
 	//	return "Leaving this page will abort the Room. Other Room Memebers will be able to see it was you that left. Do you want to continue?";
 	//}
@@ -67,7 +69,6 @@ if (Meteor.isClient) {
 			var getRoomAdmin = $('.room-admin').val();
 			
 			var optionsCount = $('.options-count').val();
-
 			
 			var adminKey = "";
 			Meteor.call("stringGen", 14, function(error, result){
@@ -79,14 +80,54 @@ if (Meteor.isClient) {
 					window.location.href = '/manageRoom/' + result;    
        			}
     		})
-		}
+		},
 	});
 	
+	Template.displayRoom.helpers({
+		//display roomCode
+		roomCode: function () {
+			var params =  Router.current().params;			
+			Meteor.call("getRoomIDByRoomID", params._id.toString(), function(error, result){
+				Session.set('roomID', result);
+    		});
+					
+			return Session.get('roomID');
+		},
+		//display admin name
+		adminName: function () {
+			var params =  Router.current().params;			
+			Meteor.call("getRoomAdminByRoomID", params._id.toString(), function(error, result){
+				Session.set('adminName', result);
+    		});
+					
+			return Session.get('adminName');
+		},
+		//display roomMembers
+		roomData: function () {
+			var params =  Router.current().params;		
+			return ReactiveMethod.call("getRoomDataArrByRoomID", params._id.toString());
+		},
+		roomSize: function () {
+			var params =  Router.current().params;	
+			return ReactiveMethod.call("getRoomSizeByRoomID", params._id.toString());
+		},
+		personID: function () {
+			var params =  Router.current().params;			
+   			return params.personID.toString();
+		},
+		options: function () {
+			var params =  Router.current().params;			
+			
+			return ReactiveMethod.call("getOptionsCountByRoomID", params._id.toString());
+		}
+    });
+
+		
 	Template.manageRoom.helpers({  
 		//display roomCode
 		roomCode: function () {
 			var params =  Router.current().params;			
-			Meteor.call("getRoomID", params._id.toString(), function(error, result){
+			Meteor.call("getRoomIDByAdminKey", params._id.toString(), function(error, result){
 				Session.set('roomID', result);
     		});
 					
@@ -108,7 +149,7 @@ if (Meteor.isClient) {
 		joinLink: function () {
 			var params =  Router.current().params;	
 			
-			Meteor.call("getRoomID", params._id.toString(), function(error, result){
+			Meteor.call("getRoomIDByAdminKey", params._id.toString(), function(error, result){
 				Session.set('roomID', result);
     		});
 					
@@ -119,42 +160,34 @@ if (Meteor.isClient) {
 		//display admin name
 		adminName: function () {
 			var params =  Router.current().params;			
-			Meteor.call("getRoomAdmin", params._id.toString(), function(error, result){
+			Meteor.call("getRoomAdminByAdminKey", params._id.toString(), function(error, result){
 				Session.set('adminName', result);
     		});
 					
 			return Session.get('adminName');
 		},
 		//display roomMembers
-		roomMembers: function () {
-			var params =  Router.current().params;			
-			Meteor.call("getNames", params._id.toString(), function(error, result){
-				Session.set('roomMembers', result);
-    		});
-			return Session.get('roomMembers');
-		},
-		memberSubmission: function () {
-			var params =  Router.current().params;			
-			Meteor.call("getHashes", params._id.toString(), function(error, result){
-				Session.set('memberSubmission', result);
-    		});
-			return Session.get('memberSubmission');
+		roomData: function () {
+			var params =  Router.current().params;		
+			return ReactiveMethod.call("getRoomDataArrByAdminKey", params._id.toString());
 		},
 		roomSize: function () {
-			var params =  Router.current().params;			
-			Meteor.call("getRoomSize", params._id.toString(), function(error, result){
-				Session.set('roomSize', result);
-    		});
-   			return Session.get('roomSize');
-		},
-		roomCapacity: function () {
-			var params =  Router.current().params;			
-			Meteor.call("getRoomCapacity", params._id.toString(), function(error, result){
-				Session.set('roomCapacity', result);
-    		});
-   			return Session.get('roomCapacity');
+			var params =  Router.current().params;	
+			return ReactiveMethod.call("getRoomSizeByAdminKey", params._id.toString());
 		},
     });
+    
+    Template.displayRoom.events({
+		'click .refresh': function () {
+			if (window.location.href.toString().indexOf("#roomMemebers") > -1) {
+				location.reload();
+			}
+			else {
+				location.reload();
+				window.location.href = window.location.href + "#roomMemebers";
+			}
+		}
+	});
     
     Template.joinRoom.events({
 		'click .submit': function () {
@@ -165,9 +198,9 @@ if (Meteor.isClient) {
 			//get the roomID
 			var newRoomMember = "" + $('.memberName').val();
 			
-			Meteor.call("addNewMember", getRoomID, newRoomMember);
-			
-			//window.location.href = '/room/' + getRoomID;
+			Meteor.call("addNewMember", getRoomID, newRoomMember, function(error, result){
+				window.location.href = '/room/' + getRoomID + "/" + result;
+    		});
 		}
 	});
     
--- server/server.js
+++ server/server.js
@@ -1,7 +1,124 @@
 import { Meteor } from 'meteor/meteor';
 
 
+//create a new collection of rooms
+Rooms = new Meteor.Collection('rooms');
+
+
 Meteor.startup(() => {
   // code to run on server at startup
 });
 
+//server side
+if (Meteor.isServer) {
+	
+	//Meteor.publish("getRoomByAdminKey", function (adminKey) {
+	//    return Rooms.find({adminKey: adminKey}, {fields: {people: 0, adminKey: 0}}, {limit: 1});
+	//});
+	
+	Meteor.publish("rooms", function () {
+	    return Rooms.find({});
+	});
+	
+
+}
+
+Meteor.methods({
+	insert:function (roomAdmin, roomSize, adminKey, optionsCount) {
+		
+		var peopleArr = [
+			{
+				name: roomAdmin, 
+				personID: Meteor.call("stringGen", 6), 
+				submitttedBit: null, 
+				randomBits: Meteor.call("stringGen", 6), 
+				hashedBits: "Waiting for submission..."
+			}
+		]
+		
+		var timeStamp = Math.floor(Date.now() / 1000);	
+		Rooms.insert({
+			roomAdmin: roomAdmin,
+			roomSize: 1,
+			optionsCount: optionsCount,
+			adminKey: adminKey,
+			peopleArr: peopleArr,
+			timeStamp: timeStamp
+		})
+    },
+    
+    stringGen:function (len) {
+			var text = "";
+			var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+			for( var i=0; i < len; i++ )
+				text += charset.charAt(Math.floor(Math.random() * charset.length));
+			return text;
+    },
+    
+	getRoomDataArrByAdminKey:function (adminKey){
+	    return Rooms.findOne({adminKey: adminKey}).peopleArr;
+	},
+
+	getRoomAdminKey:function (adminKey) {
+	    return Rooms.findOne({adminKey: adminKey}).adminKey;
+	},
+	
+	getRoomAdminByAdminKey:function (adminKey) {
+	    return Rooms.findOne({adminKey: adminKey}).peopleArr[0].name;
+	},
+	
+	getRoomIDByAdminKey:function (adminKey) {
+	    return Rooms.findOne({adminKey: adminKey})._id.toString();
+	},
+	
+	getRoomSizeByAdminKey:function (adminKey) {
+	    return Rooms.findOne({adminKey: adminKey}).roomSize;
+	},
+	
+	getRoomDataArrByRoomID:function (roomID){
+	    return Rooms.findOne({_id: roomID}).peopleArr;
+	},
+	
+	getRoomAdminByRoomID:function (roomID) {
+	    return Rooms.findOne({_id: roomID}).peopleArr[0].name;
+	},
+	
+	getRoomIDByByRoomID:function (roomID) {
+	    return Rooms.findOne({_id: roomID})._id.toString();
+	},
+	
+	getRoomSizeByRoomID:function (roomID) {
+	    return Rooms.findOne({_id: roomID}).roomSize;
+	},
+	
+	getOptionsCountByRoomID:function (roomID) {
+	    return Rooms.findOne({_id: roomID}).optionsCount;
+	},
+	
+	addNewMember:function (roomID, newMemeberName) {
+		
+	    var newPeopleArr = Rooms.findOne({_id: roomID}).peopleArr;
+	    
+	    var personID = Meteor.call("stringGen", 6)
+	    
+	    newPeopleArr.push(
+	    		{
+					name: newMemeberName, 
+					personID: personID, 
+					submitttedBit: null, 
+					randomBits: Meteor.call("stringGen", 6), 
+					hashedBits: "Waiting for submission..."
+				});
+	    
+	    var roomSize = Rooms.findOne({_id: roomID}).roomSize;
+	    
+	    roomSize++;
+	    
+		Rooms.update(
+				{ "_id" : roomID },
+				{ $set: { "peopleArr" : newPeopleArr, "roomSize": roomSize} }
+		);
+		
+		return personID;
+	},
+});
